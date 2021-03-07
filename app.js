672432f5bf23f9d9fa8fe9052ab62cb0a8a84a2a
lentqq <lentqq@gmail.com>
2021-03-08 00:40:52 +0200

    Add function to save cars in the Locale Storage

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const carButton = document.querySelector('.car-button');
 const carList = document.querySelector('.car-list');
 
 //Event Listeners
+// document.addEventListener('DOMContentLoaded', getCars);
 carButton.addEventListener('click', addCar);
 carList.addEventListener('click', deleteCar);
 //Functions
@@ -19,6 +20,8 @@ function addCar(event) {
     newCar.innerText = carInput.value;
     newCar.classList.add('car-item');
     carDiv.appendChild(newCar);
+    //Add Car To LocaleStorage
+    // saveLocalCars(carInput.value);
     //Add trash button
     const trashButton = document.createElement('button');
     trashButton.innerHTML = '<i class="fas fa-trash"></i>';
@@ -35,12 +38,67 @@ function deleteCar(e) {
     //Delete Car
     if (item.classList[0] === 'trash-btn') {
         const car = item.parentElement;
+        // removeLocaleCars(car);
         car.remove();
 
         //Animation
         // car.classList.add("fall");
+        // // removeLocaleCars(car);
         // car.addEventListener('transitionend', function () {
         //     car.remove();
         // })
     }
 };
+
+// function saveLocalCars(car) {
+//     //Check if it exists
+//     let cars;
+//     if (localStorage.getItem('cars') === null) {
+//         cars = [];
+//     } else {
+//         cars = JSON.parse(localStorage.getItem('cars'));
+//     }
+
+//     cars.push(car);
+//     localStorage.setItem('cars', JSON.stringify('cars'));
+// }
+
+// function getCars() {
+//     //Check if it exists
+//     let cars;
+//     if (localStorage.getItem('cars') === null) {
+//         cars = [];
+//     } else {
+//         cars = JSON.parse(localStorage.getItem('cars'));
+//     }
+//     cars.forEach(function (car) {
+//         //Car DIV
+//         const carDiv = document.createElement('div');
+//         carDiv.classList.add('car');
+//         //Create LI
+//         const newCar = document.createElement('li');
+//         newCar.innerText = car;
+//         newCar.classList.add('car-item');
+//         carDiv.appendChild(newCar);
+//         //Add trash button
+//         const trashButton = document.createElement('button');
+//         trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+//         trashButton.classList.add('trash-btn');
+//         carDiv.appendChild(trashButton);
+//         //Append to List
+//         carList.appendChild(carDiv);
+//     });
+// }
+
+// function removeLocaleCars(car) {
+//     //Check if it exists
+//     let cars;
+//     if (localStorage.getItem('cars') === null) {
+//         cars = [];
+//     } else {
+//         cars = JSON.parse(localStorage.getItem('cars'));
+//     }
+//     const carIndex = car.children[0].innerText;
+//     cars.splice(cars.indexOf(carIndex), 1);
+//     localStorage.setItem('cars', JSON.stringify(cars));
+// }
